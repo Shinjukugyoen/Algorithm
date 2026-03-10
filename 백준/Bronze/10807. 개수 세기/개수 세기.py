765dc22f29262f7a97fc7a8796912bb99ea9a8fd
Shinjukugyoen <kkok6506@gmail.com>
2026-03-10 12:33:34 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,4 +1,10 @@
 n = int(input())
 int_array = list(map(int, input().split()))   
-v = int(input())   
-print(int_array.count(v)) 
+v = int(input())
+v_count = 0
+
+for i in range(n):
+    if v == int_array[i]:
+        v_count += 1 
+   
+print(v_count) 
